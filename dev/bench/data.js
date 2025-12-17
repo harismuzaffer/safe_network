7d22f4c5deb28bbfa19c2c5fd9d79bf7d9794b4c
github-action-benchmark <github@users.noreply.github.com>
2025-12-17 09:26:02 +0000

    add Node memory (customSmallerIsBetter) benchmark result for 0f41a5a091167ca2caf8c15d83a3743a6ca26c38

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1765963473187,
+  "lastUpdate": 1765963562175,
   "repoUrl": "https://github.com/harismuzaffer/safe_network",
   "entries": {
     "`safe files` benchmarks": [
@@ -39,6 +39,37 @@ window.BENCHMARK_DATA = {
             "name": "ant files download",
             "value": 6.672609983689427,
             "unit": "MiB/s"
+          }
+        ]
+      }
+    ],
+    "Node memory": [
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0f41a5a091167ca2caf8c15d83a3743a6ca26c38",
+          "message": "Merge pull request #3346 from maqi/delay_replication_during_churn\n\nfeat(node): skip replication once closeup peer restart pattern detected",
+          "timestamp": "2025-12-16T22:34:35Z",
+          "tree_id": "7968cbe3bae3596139f836e289bc402c40326f9c",
+          "url": "https://github.com/harismuzaffer/safe_network/commit/0f41a5a091167ca2caf8c15d83a3743a6ca26c38"
+        },
+        "date": 1765963561788,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Peak memory w/ `safe` benchmarks",
+            "value": 183,
+            "unit": "MB"
           }
         ]
       }
