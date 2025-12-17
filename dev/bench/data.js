f092eac68bf07504e08a91be4a88ba4556c8b78a
github-action-benchmark <github@users.noreply.github.com>
2025-12-17 09:24:33 +0000

    add `safe files` benchmarks (customBiggerIsBetter) benchmark result for 0f41a5a091167ca2caf8c15d83a3743a6ca26c38

---
+++ dev/bench/data.js
@@ -0,0 +1,47 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1765963473187,
+  "repoUrl": "https://github.com/harismuzaffer/safe_network",
+  "entries": {
+    "`safe files` benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "qi.ma@maidsafe.net",
+            "name": "maqi",
+            "username": "maqi"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0f41a5a091167ca2caf8c15d83a3743a6ca26c38",
+          "message": "Merge pull request #3346 from maqi/delay_replication_during_churn\n\nfeat(node): skip replication once closeup peer restart pattern detected",
+          "timestamp": "2025-12-16T22:34:35Z",
+          "tree_id": "7968cbe3bae3596139f836e289bc402c40326f9c",
+          "url": "https://github.com/harismuzaffer/safe_network/commit/0f41a5a091167ca2caf8c15d83a3743a6ca26c38"
+        },
+        "date": 1765963472566,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "ant files upload 1mb",
+            "value": 1.0254455925914898,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files upload 10mb",
+            "value": 5.821787398377099,
+            "unit": "MiB/s"
+          },
+          {
+            "name": "ant files download",
+            "value": 6.672609983689427,
+            "unit": "MiB/s"
+          }
+        ]
+      }
+    ]
+  }
+}
